42a5dfd8baba2540ff10107cfe16dd2b7ab68250
Vadim Leonov <vleonov@edu.hse.ru>
2025-10-01 18:45:44 +0300

    benchmarks

--- lru_container/main.cpp
+++ lru_container/main.cpp
@@ -1,9 +1,12 @@
-#include "src/tests/lru_time_index_tests.h"
+#include "src/tests/lru_basic_tests.h"
 #include "src/implements/lru_time_index_container.h"
+#include "src/benchmarks/lru_basic_benchmarks.h"
 
 int main() {
     test_lru_users<LRUCacheContainer_TimeIndex>();
     test_lru_products<LRUCacheContainer_TimeIndex>();
     std::cout << "all tests success" << std::endl;
+
+    benchmark::simple_benchmark<LRUCacheContainer_TimeIndex>("output.txt");
     return 0;
 }
--- lru_container/src/benchmarks/lru_basic_benchmarks.h
+++ lru_container/src/benchmarks/lru_basic_benchmarks.h
@@ -12,6 +12,10 @@
 
 namespace benchmark {
 
+const std::vector<size_t> CACHE_SIZES = {1000, 10000, 100000};
+const size_t OPERATIONS_NUMBER = 100000;
+const int MAX_ID_SIZE = 50000;
+
 struct id_tag {};
 struct email_tag {};
 struct name_tag {};
@@ -27,21 +31,27 @@ struct User {
 };
 
 namespace generator {
-unsigned long long id_cntr = 0;
-
 std::random_device rd;
 std::mt19937 gen(rd());
 std::uniform_real_distribution<double> action_dist(0.0, 1.0);
-std::uniform_int_distribution<size_t> id_dist(0, 1000);
+std::uniform_int_distribution<int> id_dist(0, MAX_ID_SIZE);
 
 User generate_user() {
     std::string email = "email" + std::to_string(id_dist(gen));
     std::string name = "name" + std::to_string(id_dist(gen));
-    return User{++id_cntr, email, name};
+    return User{id_dist(gen), email, name};
 }
 
-void clear_id_cntr() {
-    id_cntr = 0;
+int generate_id() {
+    return id_dist(gen);
+}
+
+std::string generate_name() {
+    return "name" + std::to_string(id_dist(gen));
+}
+
+std::string generate_email() {
+    return "email" + std::to_string(id_dist(gen));
 }
 } // generator
 
@@ -70,22 +80,42 @@ void simple_benchmark(std::string &&output_filename) {
         return;
     }
 
-    output_file << std::left << std::setw(12) << "Keys count" 
+    output_file << std::left << std::setw(20) << "Operations count" 
                 << std::setw(16) << "Cache size" 
                 << std::setw(12) << "Time (ms)" 
                 << std::endl;
     output_file << std::string(50, '-') << std::endl;
-    
-    const std::vector<size_t> cache_sizes = {1000, 10000, 100000};
-    const size_t operations_number = 100000;
 
-    for (const size_t size : cache_sizes) {
+    for (const size_t size : CACHE_SIZES) {
         UserCache cache(size);
         for (size_t i = 0; i < size; ++i) {
             cache.emplace(generator::generate_user());
         }
 
+        size_t reading_operations_number = OPERATIONS_NUMBER * 4 / 5;
+        size_t writing_operations_number = OPERATIONS_NUMBER / 5;
+
         auto start_time = std::chrono::high_resolution_clock::now();
+        
+
+        for (size_t i = 0; i < reading_operations_number; ++i) {
+            cache.template get<name_tag>().find(generator::generate_name());
+            cache.template get<email_tag>().find(generator::generate_email());
+            cache.template get<id_tag>().find(generator::generate_id());
+        }
+
+        for (size_t i = 0; i < writing_operations_number; ++i) {
+            cache.emplace(generator::generate_user());
+        }
+
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+        output_file << std::left << std::setw(20) << OPERATIONS_NUMBER
+                    << std::setw(16) << size 
+                    << std::setw(12) << elapsed.count()
+                    << std::endl;
+        output_file << std::string(50, '-') << std::endl;
     }
 
 
